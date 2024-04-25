78dd875a7dd7136772bcf947ca04e78200f9b7c1
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2024-04-25 19:57:16 -0400

    rendu section en dessous main

---
+++ assets/js/mainHiking.js
@@ -0,0 +1,10 @@
+//Définitions variables du main&nav
+const mainHero_div = document.getElementById("mainHero_div");
+const h1_Balise = document.getElementById("titrePrincipal");
+// Utilisation des variables
+
+// 1. Background image du hiker
+mainHero_div.style.backgroundImage = "url('assets/img/cours1_bgHero.jpg')";
+mainHero_div.style.backgroundSize = "100% 100%";
+mainHero_div.style.backgroundRepeat = "no-repeat";
+h1_Balise.style.color = "#edb47c";
