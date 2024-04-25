aa47368c4569581ba64f2b8da7d724b80bffa162
phili <0758510@cegepoutaouais.qc.ca>
2024-04-25 10:54:32 -0400

    manque le mvt à droite

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,26 @@ const mainContainer = document.getElementsByClassName("container");
 const h1_Balise_Titre = document.body.querySelector("h1");
 const h2_Balise_Titre = document.body.querySelector("h2");
 const leadParagraphe = document.getElementsByClassName('lead');
-const p_Balise_Suivantlead = document.querySelectorAll("p")[0];
+const i_Balise_Childlead = document.querySelectorAll("i")[0];
+const p_Pagraphes_All = document.querySelectorAll("p");
+
+
 h2_Balise_Titre.addEventListener("mouseenter", () =>
 {
 h2_Balise_Titre.style.color = "white";
 h2_Balise_Titre.style.backgroundColor = "black";
-p_Balise_Suivantlead.style.color = "white";
-p_Balise_Suivantlead.style.backgroundColor = "red";
+i_Balise_Childlead.style.color = "white";
+i_Balise_Childlead.style.backgroundColor = "red";
+
+  h2_Balise_Titre.addEventListener("mouseout", () =>
+  {
+    h2_Balise_Titre.style.color = "black";
+    h2_Balise_Titre.style.backgroundColor = "white";
+    i_Balise_Childlead.style.color = "black";
+    i_Balise_Childlead.style.backgroundColor = "white";
+  });
 
 });
-
-
-
-
-
-
-
 
 // Les 2 li :
 const li_Balises = document.getElementsByClassName("list-group-item");
@@ -28,20 +32,43 @@ const li_2 = li_Balises[1];
 
 // Fonction à appeler sur le onHover du titre ou avec un event lister que nous utilisons ici :
 
-h1_Balise_Titre.addEventListener("mouseenter", function () {
+h1_Balise_Titre.addEventListener("mouseover", function () {
 
   h1_Balise_Titre.style.cursor = "pointer";
-  p_Balise_Suivantlead.style.color = "gray";
-  p_Balise_Suivantlead.style.textAlign = "right";
+  p_Pagraphes_All[1].style.color = "gray";
+  p_Pagraphes_All[1].style.textAlign = "right";
+  p_Pagraphes_All[1].style.animation = "slide, 3s ease-in-out, 1s ease-in-out,1,right";
 
   // fonction qui permet au texte de rester noir, celle-ci n'est qu'appelée si mouseenter est activé.
   h1_Balise_Titre.addEventListener("mouseout", function () {
 
-    p_Balise_Suivantlead.style.color = "black";
-    p_Balise_Suivantlead.style.textAlign = "start";
+    p_Pagraphes_All[1].style.color = "black";
+    p_Pagraphes_All[1].style.textAlign = "start";
   });
+
 });
+
+
 li_1.type = "button";
+li_2.type = "button";
+
+li_1.onclick = function ()
+{
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  this.style.backgroundColor = "black";
+  li_2.style.backgroundColor = "black";
+
+};
+li_2.onclick = function ()
+{
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+  this.style.backgroundColor = "white";
+  li_1.style.backgroundColor = "white";
+}
+
+/*
 li_1.addEventListener("mouseenter",function () {
 
   document.body.dataset.bstheme= "dark";
@@ -53,5 +80,10 @@ li_2.addEventListener("mouseenter",function () {
   document.body.dataset.bstheme= "light";
 
 });
+
+ mettre dans une fonction le tout entouré d'un if ou d'un while
+
+*/
+
 // Les deux toggle ajoute la classe au body mais la couleur ne change pas, à faire
-console.log(p_Balise_Suivantlead,leadParagraphe,li_2,li_1,mainContainer[0])
+console.log(i_Balise_Childlead,leadParagraphe,li_2,li_1,mainContainer[0])
