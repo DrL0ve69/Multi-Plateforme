ea33f5fadefb21a4788af5a61e03d4aa8c9811b3
phili <0758510@cegepoutaouais.qc.ca>
2024-04-26 18:07:05 -0400

    ajout du server.js

---
+++ assets/js/server.js
@@ -0,0 +1,9 @@
+import http from 'http';
+const server = http.createServer((req, res) =>
+{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('<!DOCTYPE html>');
+})
+const port = server.listen(8080);
+console.log('Server running at http://localhost:8080');
+console.log(`Server running at ${port}`);
