dfb4c9dfa7ed417d588cbc83dd9f79d493d577e3
phili <0758510@cegepoutaouais.qc.ca>
2024-04-26 00:44:39 -0400

    les 3 cards sont ajoutées manque les images plus section en dessous

--- assets/js/mainHiking.js
+++ assets/js/mainHiking.js
@@ -1,6 +1,7 @@
 //Définitions variables du main&nav
 const mainHero_div = document.getElementById("mainHero_div");
 const h1_Balise = document.getElementById("titrePrincipal");
+const h1_TitreSecondaire = document.getElementById("h1_sectionSecondaire");
 // Utilisation des variables
 
 // 1. Background image du hiker
@@ -8,3 +9,7 @@ mainHero_div.style.backgroundImage = "url('assets/img/cours1_bgHero.jpg')";
 mainHero_div.style.backgroundSize = "100% 100%";
 mainHero_div.style.backgroundRepeat = "no-repeat";
 h1_Balise.style.color = "#edb47c";
+h1_sectionSecondaire.style.color = "#edb47c";
+
+//Animation LOGO&Titre_h1
+gsap.from("h1", {duration: 5, x: "-1000", ease:"bounce"});
